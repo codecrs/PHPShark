be9206e3d969be090dfa56c543b287ea30c565b9
Contemplative Radicals <41048388+codecrs@users.noreply.github.com>
2018-08-01 23:14:33 +0530

    Update router.php

--- project/router.php
+++ project/router.php
@@ -4,13 +4,5 @@ namespace core{
     //Public Routers
     Router::route("/",["path" => "public"]);
     Router::route("404",["path" => "public".DS."error"]);
-    Router::route("denied",["path" => "public".DS."denied"]);
-    Router::route("logout",["path" => "public".DS."logout"]);
-    Router::route("callback",["path" => "public".DS."callback"]);
-    Router::route("test",["path" => "public".DS."test"]);
-	  Router::route("app",["path" => "public".DS."app"]);
-
-  //Member Routers
-    Router::route("member",["path" => "customer".DS."members"]);
   /****************SET UP YOUR ROUTERS HERE  */
 }
